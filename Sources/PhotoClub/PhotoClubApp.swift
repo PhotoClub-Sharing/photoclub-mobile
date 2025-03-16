8d7e3a626f557524b4254c99133565cd4abaeaaf
Morris Richman <81453549+Mcrich23@users.noreply.github.com>
2025-03-16 10:30:27 -0700

    disable auth

--- Sources/PhotoClub/PhotoClubApp.swift
+++ Sources/PhotoClub/PhotoClubApp.swift
@@ -12,25 +12,25 @@ let androidSDK = ProcessInfo.processInfo.environment["android.os.Build.VERSION.S
 /// The default implementation merely loads the `ContentView` for the app and logs a message.
 public struct RootView : View {
     @StateObject var albumManager = AlbumManager()
-    @StateObject var authManager = AuthManager()
+//    @StateObject var authManager = AuthManager()
     
     public init() {
     }
 
     public var body: some View {
         Group {
-            if authManager.isAuthenticated {
+//            if authManager.isAuthenticated {
                 AlbumsView()
-            } else {
-                AuthView()
-            }
+//            } else {
+//                AuthView()
+//            }
         }
             .task {
                 logger.log("Welcome to Skip on \(androidSDK != nil ? "Android" : "Darwin")!")
                 logger.warning("Skip app logs are viewable in the Xcode console for iOS; Android logs can be viewed in Studio or using adb logcat")
             }
             .environmentObject(albumManager)
-            .environmentObject(authManager)
+//            .environmentObject(authManager)
     }
 }
 
